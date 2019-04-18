c828f98dd0fad30ceeeb44e437ddd369771a738b
meredithcmiles <36449377+meredithcmiles@users.noreply.github.com>
2019-04-18 11:42:04 -0400

    Update findTrains.R

--- R/findTrains.R
+++ R/findTrains.R
@@ -1,34 +1,47 @@
 # scans a wav file (in tuneR's Wave format) for pulse trains
-# currently needs a 2-channel file
+# for now, only works with 2-channel files
+# must contain the stimulation in one channel and the response in another
+# uses the stimulation as basis for extraction
 
-findTrains<-function(wave){
+findTrains<-function(wave, l=100){
   twitch=wave@left
-  stim=wave@right
   samp.rate<-wave@samp.rate
-  
-  # smooth out stim
-  stim.smooth<-as.vector(env(stim, f = samp.rate, envt = "abs", msmooth=c(100, 0.8), norm = TRUE, plot=FALSE))
-  stim.smooth[stim.smooth>.5]<-1
-  stim.smooth[stim.smooth<.5]<-0
-  t.end<-length(stim)/samp.rate
-  
-  # but now we need to keep the timescales tractable
-  time<-seq(0, t.end, length.out=length(stim.smooth))
-  samples<-round(seq(1, length(twitch), length.out=length(stim.smooth)))
-  
-  smooth.out<-matrix(c(samples, time, stim.smooth), nrow=length(stim.smooth), ncol=3, dimnames=list(NULL, c("samples", "sec", "stim")))
-  
-  
-  stimpeaks<-findpeaks(smooth.out[,3], minpeakheight=0.5, minpeakdistance=175)
-  
-  train.start<-smooth.out[stimpeaks[,3]-10,1] # get start point for trains in original sample units
-  train.end<-smooth.out[stimpeaks[,4]+10,1] # get endpoint for trains in original sample units
-  
-  trains<-vector(mode="list", length=length(train.start))
-  
-  for (i in 1:length(trains)){
-    trains[[i]]<-data.frame("twitch"=twitch[train.start[i]:(train.end[i]+samp.rate)],"stim"=stim[train.start[i]:(train.end[i]+samp.rate)])
-  }
-  
-  return(trains)
+    stim=wave@right
+    
+    # smooth out stim
+    stim.smooth<-as.vector(env(stim, f = samp.rate, envt = "abs", msmooth=c(125, 0.8), norm = TRUE, plot=FALSE))
+    stim.smooth[stim.smooth>.5]<-1
+    stim.smooth[stim.smooth<.5]<-0
+    t.end<-length(stim)/samp.rate
+    
+    # but now we need to keep the timescales tractable
+    time<-seq(0, t.end, length.out=length(stim.smooth))
+    samples<-round(seq(1, length(twitch), length.out=length(stim.smooth)))
+    
+    smooth.out<-matrix(c(samples, time, stim.smooth), nrow=length(stim.smooth), ncol=3, dimnames=list(NULL, c("samples", "sec", "stim")))
+    
+    
+    stimpeaks<-findpeaks(smooth.out[,3], minpeakheight=0.5, minpeakdistance=100)
+    
+    ntrains<-nrow(stimpeaks)
+    
+    train.start<-smooth.out[stimpeaks[,3]-10,1] # get start point for trains in original sample units
+    train.end<-smooth.out[stimpeaks[,4]+l,1] # get endpoint for trains in original sample units
+    
+    twitches<-vector(mode="list", length=ntrains)
+    stimulations<-vector(mode="list", length=ntrains)
+    baseline<-vector(mode="numeric", length=ntrains)
+    
+    for (i in 1:ntrains){
+      start<-train.start[i]
+      end<-train.end[i]
+      twitch.out<-twitch[start:end]
+      stim.out<-stim[start:end]
+      
+      twitches[[i]]<-twitch.out
+      stimulations[[i]]<-stim.out
+      baseline[i]<-mean(twitch[start+200:start+1000])
+    }
+  return(list("twitch"=twitches, "stim"=stimulations, "baseline"=baseline))
 }
+
