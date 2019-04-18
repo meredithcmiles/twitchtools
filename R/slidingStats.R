91c49132c2a662906c25aa74ea0d45f9ccdcc020
meredithcmiles <36449377+meredithcmiles@users.noreply.github.com>
2019-04-18 09:00:32 -0400

    Update and rename sliding.stats.R to slidingStats.R

--- R/slidingStats.R
+++ R/slidingStats.R
@@ -3,34 +3,35 @@
 # user supplies window length (default=2000 samples) and overlap % (default=0.5)
 # mostly used by other functions
 
-sliding.stats<-function(wav, wl=2000, prop=0.5){
-  
+slidingStats<-function(wav, wl=200, prop=0.8){
   len<-round(length(wav), digits=-3)
-  i<-1
   nsteps<-round((1/prop)*round(len/wl))
   output<-matrix(NA, nrow=nsteps, ncol=3)
-  colnames(output)=c("n", "avg.dB", "stdev")
+  colnames(output)<-c("n", "avg.dB", "stdev")
   
   windows<-matrix(0, nrow=wl, ncol=nsteps)
   
   for (i in 1:nsteps){
-    if(i==1){ 
-      start=1
-      end=wl
+    if(i==1){
+      
+      start<-1
+      end<-wl
+      
     } else {
-      start=end-prop*wl
-      end=start+wl-1
+      
+      start<-end-prop*wl
+      end<-start+wl-1
+      
     }
     
     window<-wav[start:end]
-    windows[,i]=window
+    windows[,i]<-window
     
-    output[i,1]=i
-    output[i,2]=mean(window)
-    output[i,3]=sd(window)
-    i=i+1
+    output[i,1]<-i
+    output[i,2]<-mean(window)
+    output[i,3]<-sd(window)
   }
   
-  return(list("output"=output, "windows"=windows))
+  return(list(output, windows))
   
 }
