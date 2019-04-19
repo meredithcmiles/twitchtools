e104faa2ae455583ba8fa62af33a411e4cbc8bea
meredithcmiles <36449377+meredithcmiles@users.noreply.github.com>
2019-04-19 11:59:45 -0400

    Update getPeaks.R

--- R/getPeaks.R
+++ R/getPeaks.R
@@ -1,20 +1,42 @@
-getPeaks<-function(train, peakdist){
-  thresh<-min(train)+(max(train)-min(train))/1.8
-  valleyset<-getValleys(train, peakdist)
+getPeaks<-function(twitch, peakdist, plot=c(FALSE, TRUE)){
+  thresh<-min(twitch)+(max(twitch)-min(twitch))/1.8
+  valleyset<-getValleys(twitch, peakdist)
+  valleyset<-valleyset[order(valleyset[,2]),]
   valley.loc<-valleyset[,2]
+  n<-length(valley.loc)
   
-  # search for a peak after each valley
-  start.loc<-valley.loc[1:length(valley.loc)-1]
-  end.loc<-valley.loc[2:length(valley.loc)]
+  # search for a peak between each of the valleys
+  start.loc<-valley.loc[1:n-1]
+  end.loc<-valley.loc[2:n]
+  search.window<-matrix(c(start.loc, end.loc), ncol=2, nrow=n-1)
   
-  peakset<-matrix(NA, nrow=length(start.loc), ncol=4)
+  peakset<-matrix(NA, nrow=n-1, ncol=2)
   
-  for (i in 1:length(start.loc)){
-    start<-start.loc[i]
-    end<-end.loc[i]
+  for (i in 1:nrow(peakset)){
+    start<-search.window[i,1]
+    end<-search.window[i,2]
     
-    peakset[i,]<-findpeaks(train[start:end], minpeakheight = thresh, npeaks=1)
+    slice<-twitch[start:end]
+    index<-start:end
+    
+    peaks<-findpeaks(twitch[start:end], minpeakheight = thresh, minpeakdistance=100)
+    peaks[,2]<-peaks[,2]+start
+
+    peak.height<-max(slice)
+    peak.loc<-index[which(slice==max(slice))]
+    
+    peakset[i,1]<-peak.height
+    peakset[i,2]<-peak.loc
   }
-  valleyset<-valleyset[1:length(peakset),]
-  return(list("peaks"=peakset, "valleys"=valleyset))
+  
+  valleyset<-valleyset[1:n-1,]
+  
+  if (plot==TRUE){
+    plot(twitch, type="l")
+    abline(v=peakset[,2], col="red")
+    abline(v=valleyset[,2], col="blue")
+    return(list("peaks"=peakset, "valleys"=valleyset))
+  } else if (plot==FALSE){
+    return(list("peaks"=peakset, "valleys"=valleyset))
+  }
 }
