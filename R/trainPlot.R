60876efb37d819706bd78079d59d5b56564f3867
meredithcmiles <36449377+meredithcmiles@users.noreply.github.com>
2019-04-21 15:19:33 -0400

    Update trainPlot.R

--- R/trainPlot.R
+++ R/trainPlot.R
@@ -1,38 +1,37 @@
-trainPlot<-function(train, peakdist=225){
+trainPlot<-function(train, line=FALSE, ...){
   twitch<-train$twitch
-  peaks<-getPeaks(twitch, peakdist, plot=FALSE)
+  stim<-train$stim
+  name<-train$name
+  hairy<-paste("hawo", c(1:12), sep="")
+  downy<-paste("dowo", c(1:12), sep="")
+  accel<-c(hairy, downy)
+  peaks<-getPeaks(train=train, ...)
   peakset<-peaks$peaks
   valleyset<-peaks$valleys
+  baseline<-peaks$baseline$moving
   
-  npeaks<-nrow(peakset)
-  
-  valley0<-valleyset[1:npeaks-1,2]
-  valleyf<-valleyset[2:npeaks,2]
-  valleyspan<-valleyf-valley0
-  blspan<-quantile(valleyspan, probs=.8)
-  
-  bl1.start<-valley0[1]-blspan*2
-  bl1.start[bl1.start<1]<-1
-  bl1.end<-bl1.start+blspan
-  
-  bl2.start<-max(peakset[,2])+blspan*2
-  bl2.end<-bl2.start+blspan
-  
-  bl1<-mean(twitch[bl1.start:bl1.end])
-  bl2<-mean(twitch[bl2.start:bl2.end])
+  bl1.start<-peaks$baseline$bl1[1]
+  bl1.end<-peaks$baseline$bl1[2]
+  bl2.start<-peaks$baseline$bl2[1]
+  bl2.end<-peaks$baseline$bl2[2]
   
   blcol<-rgb(225/255,226/255,96/255,.5)
   xleft<-c(bl1.start, bl2.start)
   xright<-c(bl1.end, bl2.end)
-  ytop<-c(max(twitch[bl1.start:bl1.end]), max(twitch[bl2.start:bl2.end]))
-  ybottom<-c(min(twitch[bl1.start:bl1.end]), min(twitch[bl2.start:bl2.end]))
+  ytop<-c(peaks$baseline$bl1[4], peaks$baseline$bl2[4])
+  ybottom<-c(peaks$baseline$bl1[3], peaks$baseline$bl2[3])
   
-  plot(twitch)
+  if(line==TRUE){
+    plot(twitch, type="l", xlab=name)
+  } else {
+    plot(twitch, xlab=name)
+  }
+  
   abline(v=peakset[,2], col="red")
   abline(v=valleyset[,2], col="blue")
   
   rect(xleft, ybottom, xright, ytop, col=c(blcol,blcol))
-  segments(bl1.start+blspan/2, bl1, bl2.start+blspan/2, bl2)
+  segments(bl1.start+blspan/2, baseline[1], bl2.start+blspan/2, baseline[length(baseline)])
   textx<-((bl2.end-bl1.end)/2)+100
   texty<-abs(bl1-bl2)/2 + min(bl1, bl2)
   text<-"sliding baseline"
