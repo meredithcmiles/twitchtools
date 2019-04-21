42748fc590b9cb61edf927c47443c350933b749f
meredithcmiles <36449377+meredithcmiles@users.noreply.github.com>
2019-04-21 15:14:00 -0400

    Update getValleys.R

--- R/getValleys.R
+++ R/getValleys.R
@@ -1,9 +1,25 @@
 # internal function call
 # extract valleys from muscle twitch train
 
-getValleys<-function(train, peakdist=400){
-  train<-train*-1
-  thresh<-min(train)+(max(train)-min(train))/2
-  valleyset<-findpeaks(train.inv,minpeakheight=thresh, minpeakdistance = peakdist, sortstr = FALSE)
+getValleys<-function(train, thresh=1.25, peakdist=225){
+  stim<-train$stim
+  twitch<-train$twitch*-1
+  threshold<-min(twitch)+(max(twitch)-min(twitch))/thresh
+  valleyset<-findpeaks(twitch,minpeakheight=threshold, minpeakdistance = peakdist, sortstr = FALSE)
+  valleyset<-valleyset[order(valleyset[,2]),]
+  valleyset[,1]<-valleyset[,1]*-1
+  
+  stimset<-findStim(stim)
+  stim0<-min(stimset)
+  stimf<-max(stimset)
+  
+  stim0<-stim0-50
+  valleyset<-valleyset[!valleyset[,2]<stim0,]
+  
+  ind<-which(valleyset[,2]>stimf)
+  if(length(ind)>1){
+    drop<-min(ind)+1
+    valleyset<-valleyset[1:drop,]
+  }
   return(valleyset)
 }
