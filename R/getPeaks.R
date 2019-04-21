3cf1fd6de53d1b9a6bd40113dffecbcf4b702dee
meredithcmiles <36449377+meredithcmiles@users.noreply.github.com>
2019-04-21 15:22:44 -0400

    Update getPeaks.R

--- R/getPeaks.R
+++ R/getPeaks.R
@@ -1,7 +1,11 @@
-getPeaks<-function(twitch, peakdist, plot=c(FALSE, TRUE)){
-  thresh<-min(twitch)+(max(twitch)-min(twitch))/1.8
-  valleyset<-getValleys(twitch, peakdist)
-  valleyset<-valleyset[order(valleyset[,2]),]
+getPeaks<-function(train, thresh=2.25, peakdist=225){
+  twitch<-train$twitch
+  stim<-train$stim
+  name<-train$name
+  hairy<-paste("hawo", c(1:12), sep="")
+  downy<-paste("dowo", c(1:12), sep="")
+  accel<-c(hairy, downy)
+  valleyset<-getValleys(train=train, thresh=thresh, peakdist=peakdist)
   valley.loc<-valleyset[,2]
   n<-length(valley.loc)
   
@@ -9,7 +13,7 @@ getPeaks<-function(twitch, peakdist, plot=c(FALSE, TRUE)){
   start.loc<-valley.loc[1:n-1]
   end.loc<-valley.loc[2:n]
   search.window<-matrix(c(start.loc, end.loc), ncol=2, nrow=n-1)
-  
+
   peakset<-matrix(NA, nrow=n-1, ncol=2)
   
   for (i in 1:nrow(peakset)){
@@ -19,24 +23,40 @@ getPeaks<-function(twitch, peakdist, plot=c(FALSE, TRUE)){
     slice<-twitch[start:end]
     index<-start:end
     
-    peaks<-findpeaks(twitch[start:end], minpeakheight = thresh, minpeakdistance=100)
+    peaks<-findpeaks(twitch[start:end], minpeakheight = quantile(twitch, probs=0.1), minpeakdistance=100)
     peaks[,2]<-peaks[,2]+start
-
-    peak.height<-max(slice)
-    peak.loc<-index[which(slice==max(slice))]
+    
+    peak.loc<-min(index[which(slice==max(slice))])
+    peak.height<-max(peaks[,1])
     
     peakset[i,1]<-peak.height
     peakset[i,2]<-peak.loc
   }
+
+  blspan<-500
   
-  valleyset<-valleyset[1:n-1,]
+  bl1.end<-min(valleyset[,2])-blspan
+  bl1.start<-bl1.end-blspan
   
-  if (plot==TRUE){
-    plot(twitch, type="l")
-    abline(v=peakset[,2], col="red")
-    abline(v=valleyset[,2], col="blue")
-    return(list("peaks"=peakset, "valleys"=valleyset))
-  } else if (plot==FALSE){
-    return(list("peaks"=peakset, "valleys"=valleyset))
-  }
+  bl2.start<-max(peakset[,2])+blspan*1.5
+  bl2.end<-bl2.start+blspan
+  
+  bl1.start[bl1.start<1]<-1
+  
+  bl1<-mean(twitch[bl1.start:bl1.end])
+  bl2<-mean(twitch[bl2.start:bl2.end])
+  
+  baseline<-seq(bl1, bl2, length.out=nrow(valleyset))
+
+  bl1.max<-max(twitch[bl1.start:bl1.end])
+  bl1.min<-min(twitch[bl1.start:bl1.end])
+  bl2.max<-max(twitch[bl2.start:bl2.end])
+  bl2.min<-min(twitch[bl2.start:bl2.end])
+  
+  bl1<-c(bl1.start, bl1.end, bl1.min, bl1.max)
+  bl2<-c(bl2.start, bl2.end, bl2.min, bl2.max)
+  
+  baseline<-list("moving"=baseline, "static"=mean(c(bl1, bl2)), "bl1"=bl1, "bl2"=bl2)
+  
+    return(list("peaks"=peakset, "valleys"=valleyset, "baseline"=baseline))
 }
