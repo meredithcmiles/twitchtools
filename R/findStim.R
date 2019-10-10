66e5c719fff66ab92477db77fd19f1ee5f850f5f
Meredith C. Miles <meredith_miles@brown.edu>
2019-10-10 10:35:05 -0400

    Update findStim.R

--- R/findStim.R
+++ R/findStim.R
@@ -1,7 +1,11 @@
 # takes a stimulation train and locates the time of each stimulation
 
 findStim<-function(stim){
+  # detection based on threshold at stim trace's midpoint:
   thresh<-min(stim)+(max(stim)-min(stim))/2
+  
+  # correction to deal with y-inverted traces:
+  
   n.over <- length(stim[stim>thresh])
   n.under <- length(stim[stim<thresh])
 
@@ -11,7 +15,10 @@ findStim<-function(stim){
     stim <- scale(stim, center=0.5)[,1]
   }
 
-  stimpeaks<-findpeaks(stim, minpeakheight=thresh, minpeakdistance=100)
+  # deploy peakfinder from pracma
+  stimpeaks<-pracma::findpeaks(stim, minpeakheight=thresh, minpeakdistance=100)
   t.stim<-stimpeaks[,2]
+  
   return(t.stim[order(t.stim)])
+  
 }
