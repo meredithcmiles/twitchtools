dee28a38bfba3f03d0e6225398e0197089da7d54
Meredith C. Miles <meredith_miles@brown.edu>
2019-10-10 10:33:12 -0400

    Update findStim.R

--- R/findStim.R
+++ R/findStim.R
@@ -1,9 +1,17 @@
-# internal function call
-# peakfinder tuned to detect stimulations from channel 2
+# takes a stimulation train and locates the time of each stimulation
 
 findStim<-function(stim){
   thresh<-min(stim)+(max(stim)-min(stim))/2
-  stimpeaks<-findpeaks(stim, minpeakheight=0.5, minpeakdistance=100)
+  n.over <- length(stim[stim>thresh])
+  n.under <- length(stim[stim<thresh])
+
+  if(n.over > n.under){
+    stim <- -stim + max(stim)
+    scl <- 1/max(stim)
+    stim <- scale(stim, center=0.5)[,1]
+  }
+
+  stimpeaks<-findpeaks(stim, minpeakheight=thresh, minpeakdistance=100)
   t.stim<-stimpeaks[,2]
   return(t.stim[order(t.stim)])
 }
